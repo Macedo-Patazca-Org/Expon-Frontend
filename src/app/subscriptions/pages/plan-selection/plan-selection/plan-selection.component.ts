1a84a802433dfca4c4f8a8c6beedaf7e26814c94
SantosPatazca <u20201c269@upc.edu.pe>
2025-06-06 00:27:16 -0500

    feat: add initial plan section layout

--- src/app/subscriptions/pages/plan-selection/plan-selection/plan-selection.component.ts
+++ src/app/subscriptions/pages/plan-selection/plan-selection/plan-selection.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-plan-selection',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
+  ],
   templateUrl: './plan-selection.component.html',
-  styleUrl: './plan-selection.component.css'
+  styleUrls: ['./plan-selection.component.css']
 })
-export class PlanSelectionComponent {
-
-}
+export class PlanSelectionComponent {}
